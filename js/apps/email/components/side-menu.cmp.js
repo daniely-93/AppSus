6f4fa3759e4f4ae60f8f1e6955291424f65f734c
daniely-93 <danielyona93@gmail.com>
2019-11-09 16:58:35 +0200

    Fix bug designs

--- js/apps/email/components/side-menu.cmp.js
+++ js/apps/email/components/side-menu.cmp.js
@@ -7,7 +7,7 @@ export default {
             <li :class="{active : active === 'inbox' ,  bold : unread && active === 'inbox' }" class="side-menu-item" @click="onDirClick('inbox')"><i class="fa fa-inbox"></i> Inbox <span v-if="unread && active === 'inbox'">({{ unread }})</span></li>
             <li :class="{active : active === 'sent' ,  bold : unread && active === 'sent' }" class="side-menu-item" @click="onDirClick('sent')"><i class="fa fa-paper-plane"></i> Sent <span v-if="unread && active === 'sent'">({{ unread }})</span></li>
             <li :class="{active : active === 'starred' ,  bold : unread && active === 'starred' }" class="side-menu-item" @click="onDirClick('starred')"><i class="fa fa-star"></i > Starred <span v-if="unread && active === 'starred'">({{ unread }})</span></li>
-            <li :class="{active : active === 'draft' ,  bold : unread && active === 'draft' }" class="side-menu-item" @click="onDirClick('drafts')"><i class="fa fa-sticky-note"></i> Drafts <span v-if="unread && active === 'drafts'">({{ unread }})</span></li>
+            <li :class="{active : active === 'drafts' ,  bold : unread && active === 'draft' }" class="side-menu-item" @click="onDirClick('drafts')"><i class="fa fa-sticky-note"></i> Drafts <span v-if="unread && active === 'drafts'">({{ unread }})</span></li>
             <li :class="{active : active === 'trash' ,  bold : unread && active === 'trash' }" class="side-menu-item" @click="onDirClick('trash')"><i class="fa fa-trash"></i> Trash <span v-if="unread && active === 'trash'">({{ unread }})</span></li>
         </ul>
     </div>`,
--- js/apps/keeps/components/todo-note.cmp.js
+++ js/apps/keeps/components/todo-note.cmp.js
@@ -1,10 +1,10 @@
 export default{
     props:['info'],
     template:`
-    <div class="todo-cmp note-cmp">
-        <h3>Your todos are:</h3>
+    <div class="note">
+        <h3>Todos</h3>
         <h6 v-for="(todo , idx) in todos" @click="$event.target.classList.toggle('done')">{{idx+1}}) {{todo}}
-            <button type="button" class="delete-todo"  @click="$event.target.parentNode.classList.toggle('hide')"> X </button>
+            <button type="button" class="delete-todo" @click="$event.target.parentNode.classList.toggle('hide')"> X </button>
         </h6>
     </div>
     `,
@@ -23,6 +23,9 @@ export default{
         },
         isDone(idx){
             return this.doneTodos.contains(idx);
+        },
+        toggleDone(){
+
         }
     }
 }
