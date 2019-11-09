4b4d70e4b2e6831d482342b90bf0a3a01b7410e5
daniely-93 <danielyona93@gmail.com>
2019-11-09 15:18:09 +0200

    Fix active bug

--- js/apps/keeps/components/add-note.cmp.js
+++ js/apps/keeps/components/add-note.cmp.js
@@ -5,12 +5,12 @@ export default {
     <div class="notes-maker">
         <input type="text" class="url-txt" v-model="txt" placeholder="whats on your mind..."></input>
         <div class="btn-notes">
-            <button type="button" class="noteTxt btn-note-type" @click="changeNote('noteTxt')" v-bind:class="{active: isActive('noteTxt')}"><i class="fa fa-font"></i></button>
-            <button type="button" class="noteImage btn-note-type" @click="changeNote('noteImage')" v-bind:class="{active: isActive('noteImage')}"><i class="fa fa-image"></i></button>
-            <button type="button" class="noteVideo btn-note-type" @click="changeNote('noteVideo')" v-bind:class="{active: isActive('noteVideo')}"><i class="fab fa-youtube"></i></button>
-            <button type="button" class="noteSound btn-note-type" @click="changeNote('noteSound')" v-bind:class="{active: isActive('noteSound')}"><i class="fa fa-volume-up"></i></button>
-            <button type="button" class="noteTodo btn-note-type" @click="changeNote('noteTodo')" v-bind:class="{active: isActive('noteTodo')}"><i class="fa fa-list"></i></button>
-            <button type="button" class="noteTodo btn-note-type" @click="addNote"><i class="fa fa-download save"></i></button>
+            <button type="button" class="btn-note-type" @click="changeNote('noteTxt')" v-bind:class="{'note-type-active': type === 'noteTxt'}"><i class="fa fa-font"></i></button>
+            <button type="button" class="btn-note-type" @click="changeNote('noteImage')" v-bind:class="{'note-type-active': type === 'noteImage'}"><i class="fa fa-image"></i></button>
+            <button type="button" class="btn-note-type" @click="changeNote('noteVideo')" v-bind:class="{'note-type-active': type === 'noteVideo'}"><i class="fab fa-youtube"></i></button>
+            <button type="button" class="btn-note-type" @click="changeNote('noteSound')" v-bind:class="{'note-type-active': type === 'noteSound'}"><i class="fa fa-volume-up"></i></button>
+            <button type="button" class="btn-note-type" @click="changeNote('noteTodo')" v-bind:class="{'note-type-active': type === 'noteTodo'}"><i class="fa fa-list"></i></button>
+            <button type="button" class="btn-note-type" @click="addNote"><i class="fa fa-download save"></i></button>
         </div>
     </div>
     `,
