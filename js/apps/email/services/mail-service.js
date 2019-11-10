0fbef915b62811a587be7a66a10c96f5be069c31
daniely-93 <danielyona93@gmail.com>
2019-11-10 20:57:04 +0200

    Fix local storage bug

--- js/apps/email/services/mail-service.js
+++ js/apps/email/services/mail-service.js
@@ -1,5 +1,7 @@
 import utilService from '../../../services/utils-service.js';
 
+const MAILS_KEY = 'mails';
+
 export default {
     sendMail,
     getDir,
@@ -11,78 +13,95 @@ export default {
 }
 
 
-var sent = [
-    { id: 'h2Js9bCa', to: 'benny@email.com', from: 'daniel@daniel.com', subject: 'Hello', body: 'Whats up?', isRead: true, isStarred: false, sentAt: 1551133155101 }
-]
 
-var drafts = [];
-var trash = [];
-var starred = [];
-var inbox = _createInbox();
+var mailbox = {
+    inbox: [],
+    sent: [
+        { id: 'h2Js9bCa', to: 'benny@email.com', from: 'daniel@daniel.com', subject: 'Hello', body: 'Whats up?', isRead: true, isStarred: false, sentAt: 1551133155101 }
+    ],
+    starred: [],
+    drafts: [],
+    trash: []
+}
 
-function _createInbox() {
-    let inbox = []
+mailbox = _createMailbox();
+_saveMailbox();
+
+function _createMailbox() {
+    let currMailbox = _loadMailbox();
+    console.log(currMailbox);
+    if (currMailbox && currMailbox.inbox.length) return currMailbox;
+    currMailbox = {
+        inbox: [],
+        sent: [],
+        starred: [],
+        drafts: [],
+        trash: []
+    }
     for (let i = 0; i < 25; i++) {
         let currMail = _createMail();
-        inbox.push(currMail);
-        if(currMail.isStarred) starred.push(currMail);
+        currMailbox.inbox.push(currMail);
+        if (currMail.isStarred) currMailbox.starred.push(currMail);
     }
-    return inbox;
+    return currMailbox;
 }
 
 function getMailById(id) {
     return new Promise(resolve => {
-        let resMail = inbox.filter(mail => mail.id === id)
-        if (!resMail.length) resMail = sent.filter(mail => mail.id === id)
-        if (!resMail.length) resMail = trash.filter(mail => mail.id === id)
+        let resMail = mailbox.inbox.filter(mail => mail.id === id)
+        if (!resMail.length) resMail = mailbox.sent.filter(mail => mail.id === id)
+        if (!resMail.length) resMail = mailbox.trash.filter(mail => mail.id === id)
         resolve(resMail[0]);
     })
 }
 
 function toggleStar(id) {
     return new Promise(resolve => {
-        let mail = inbox.find(mail => mail.id === id)
-        if (!mail) mail = sent.find(mail => mail.id === id)
-        if (!mail) mail = trash.find(mail => mail.id === id)
+        let mail = mailbox.inbox.find(mail => mail.id === id)
+        if (!mail) mail = mailbox.sent.find(mail => mail.id === id)
+        if (!mail) mail = mailbox.trash.find(mail => mail.id === id)
         if (mail.isStarred) {
-            let starredIdx = starred.findIndex(resMail => resMail.id === mail.id);
+            let starredIdx = mailbox.starred.findIndex(resMail => resMail.id === mail.id);
             mail.isStarred = false;
-            starred.splice(starred[starredIdx], 1);
+            mailbox.starred.splice(mailbox.starred[starredIdx], 1);
             return;
         }
         if (mail.deletedFrom) return;
         mail.isStarred = true;
-        resolve(starred.unshift(mail));
+        _saveMailbox();
+        resolve(mailbox.starred.unshift(mail));
     })
 }
 
 function deleteMail(dir, id) {
     return getDir(dir).then(arr => {
-        if (arr === inbox || arr === sent) {
+        if (arr === mailbox.inbox || arr === mailbox.sent) {
             let mail = arr.find(mail => mail.id === id);
             mail.deletedFrom = dir;
-            trash.unshift(mail);
+            mailbox.trash.unshift(mail);
         }
         let mailIdx = arr.findIndex(mail => mail.id === id);
         // toggleStar(id);
         arr.splice(mailIdx, 1);
+        _saveMailbox();
     })
 }
 
 function recoverMail(id) {
-    let deletedMail = trash.find(mail => mail.id === id);
+    let deletedMail = mailbox.trash.find(mail => mail.id === id);
     return getDir(deletedMail.deletedFrom).then(arr => {
         delete deletedMail.deletedFrom
         arr.unshift(deletedMail);
-        let mailIdx = trash.findIndex(mail => mail.id === id);
-        trash.splice(mailIdx, 1);
+        let mailIdx = mailbox.trash.findIndex(mail => mail.id === id);
+        mailbox.trash.splice(mailIdx, 1);
+        _saveMailbox();
     })
 }
 
 function sendMail(mail) {
     let id = utilService.getRandomId();
     let { to, subject, body, sentAt } = mail;
-    sent.unshift({
+    mailbox.sent.unshift({
         id,
         to,
         subject,
@@ -91,7 +110,7 @@ function sendMail(mail) {
         sentAt
     })
 
-    inbox.unshift({
+    mailbox.inbox.unshift({
         id,
         to,
         subject,
@@ -99,15 +118,16 @@ function sendMail(mail) {
         isRead: false,
         sentAt
     })
+    _saveMailbox();
     return new Promise((resolve, reject) => {
-        resolve(sent[0])
+        resolve(mailbox.sent[0])
         reject(null)
     })
 }
 
 function saveMail(mail) {
     let { to, subject, body, sentAt } = mail;
-    drafts.unshift({
+    mailbox.drafts.unshift({
         to,
         subject,
         body,
@@ -118,7 +138,7 @@ function saveMail(mail) {
 }
 
 function getDir(dir) {
-    return Promise.resolve(dir === 'inbox' ? inbox : dir === 'sent' ? sent : dir === 'drafts' ? drafts : dir === 'trash' ? trash : dir === 'starred' ? starred : null);
+    return Promise.resolve(dir === 'inbox' ? mailbox.inbox : dir === 'sent' ? mailbox.sent : dir === 'drafts' ? mailbox.drafts : dir === 'trash' ? mailbox.trash : dir === 'starred' ? mailbox.starred : null);
 }
 
 function _createMail() {
@@ -132,4 +152,12 @@ function _createMail() {
         isStarred: Math.random() >= 0.5,
         sentAt: 1551133900000 + Math.floor(Math.random() * 100000)
     }
+}
+
+function _saveMailbox() {
+    utilService.saveToStorage(MAILS_KEY, mailbox)
+}
+
+function _loadMailbox() {
+    return utilService.loadFromStorage(MAILS_KEY);
 }
